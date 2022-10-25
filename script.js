57ff29a6701c6aa0100eac9d2323656e36d402da
Arthur Pouteau <arthur.pouteau92@gmail.com>
2022-10-25 14:16:09 +0200

    :construction: CREATE pages & finished menu

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ btnMenu.addEventListener('click', function(){
     document.getElementById('menu-transition').classList.toggle('show');
     document.getElementById('menu').classList.toggle('show');
     setTimeout(() => {document.getElementById('btn-menu').classList.toggle('show')}, 550);
+    
 });
