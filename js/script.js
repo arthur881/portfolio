343320535a7a84bcfdfd4a7f802caa4953d41658
Arthur Pouteau <arthur.pouteau92@gmail.com>
2023-04-15 21:02:56 +0200

    :sparkles: add custom cursor (need to improve heroSection)

--- js/script.js
+++ js/script.js
@@ -6,5 +6,26 @@ btnMenu.addEventListener('click', function(){
     setTimeout(() => {document.getElementById('btn-menu').classList.toggle('show')}, 550);
 });
 
+let cursor = document.querySelector(".cursor");
 
+document.addEventListener("mousemove", moveCursor)
 
+function moveCursor(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+}
+
+let links4cursor = Array.from(document.querySelectorAll("a"));
+links4cursor.push(btnMenu)
+
+links4cursor.forEach(link => {
+    link.addEventListener("mouseover", function() {
+        cursor.classList.add("grow");
+    });
+    link.addEventListener("mouseleave", function() {
+        cursor.classList.remove("grow");
+    });
+});
